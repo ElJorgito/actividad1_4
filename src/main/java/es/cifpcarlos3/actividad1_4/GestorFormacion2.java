fbc8fcc6580790544265f7d7f582c04ec9daf520
Jorge <11655449@alu.murciaeduca.es>
2025-10-16 19:53:47 +0200

    Clases 1 cambio 5

--- src/main/java/es/cifpcarlos3/actividad1_4/GestorFormacion2.java
+++ src/main/java/es/cifpcarlos3/actividad1_4/GestorFormacion2.java
@@ -1,11 +1,25 @@
 package es.cifpcarlos3.actividad1_4;
 
+import es.cifpcarlos3.actividad1_4.vo.FamiliaProfesional;
+
 public class GestorFormacion2 {
     public static void main(String[] args) {
         if (args.length != 2) {
-            System.err.println("Formato de argumentos incorrecto");
+            System.err.println("Uso: java GestorInformacion2 <codigo_familia> <codigo-grado>");
             return;
         }
+        String codigo_familia = args[0].trim().toUpperCase();
+        String codigo_grado = args[1].trim().toUpperCase();
 
+        FamiliaProfesional familia = cargarFamilia(codigo_familia);
+
+        if (familia == null) {
+            System.out.println("Familia no encontrada");
+            return;
+        }
+    }
+    private static FamiliaProfesional cargarFamilia(String codigo_familia) {
+        String ruta = "src/main/java/es/cifpcarlos3/actividad1_4/familia_profesional.dat";
+        return null;
     }
 }
